208e9438caf1270b82e97865bdb97c1387b69858
kabeerchhllar <kabeer.chhillar@gmail.com>
2019-06-07 08:46:52 +0530

    first programs

--- BooksWithSql.py
+++ BooksWithSql.py
@@ -1,13 +1,5 @@
 import DataSql
 
-rFile = open("books.txt", "r")
-#bookLIST = rFile.readlines()
-bookLIST = [line.rstrip() for line in rFile]
-print bookLIST
-rFile.close()
-
-myFile = open("books.txt","a")
-
 #
 #   MyBook Schema :
 #   id integer PRIMARY KEY
@@ -16,85 +8,45 @@ myFile = open("books.txt","a")
 #   year integer
 #   doIHaveIt boolean
 
+def add_books(conn,book):
+    sql = ''' INSERT INTO my_book (name,author,year,DoIHaveIt)
+                VALUES(?,?,?,?); '''
+    cur = conn.cursor()
+    cur.execute(sql,book)
+    return cur.lastrowid
+
+
 def create_my_book_table(conn):
     sql_create_books_table = """
-                            CREATE TABLE IF NOT EXISTS projects (  id integer PRIMARY KEY,
+                            CREATE TABLE IF NOT EXISTS my_book (  
+                            id integer PRIMARY KEY,
                             name text NOT NULL,
                             author text,
                             year integer,
-                            DoIHaveIt integer
-                            )"""
+                            DoIHaveIt text ); """
     if conn is not None:
         DataSql.create_table(conn,sql_create_books_table)
     else:
         print("Error! cannot create the database connection.")
 
-def input_destinations(user_input):
+def input_destinations(user_input,connection):
     if user_input == "0":
-        connection = DataSql.create_database("myBooks.db")
         create_my_book_table(connection)
-        return connection
-    if user_input == "1":
-        add_book()
-    elif user_input == "2":
-        boo = str(raw_input("Enter the name"))
-        print boo
-        if boo in bookLIST:
-            print("yeahhh .. I found the book")
-        else :
-            print("nooooo .... Sorry Cant find it")
-    elif user_input == "3":
-        print bookLIST
-    elif user_input == "4":
-        bo = str(raw_input("Enter the name"))
-        if bo in bookLIST:
-            index = bookLIST.index(bo)
-            del bookLIST[index]
-        else:
-            print "No such book found"
-            return
-        with open("books.txt", "r") as f:
-            lines = f.readlines()
-        with open("books.txt", "w") as f:
-            for line in lines:
-                if line.strip("\n") != bo:
-                    f.write(line)
-    elif user_input == "5":
-        b = str(raw_input("Enter the substring name"))
-        for book in bookLIST:
-            if b in book:
-                print book
-    elif user_input == "6":
-        year = str(raw_input("Enter the year"))
-        for book in bookLIST:
-            splitStr = book.split(", ")
-            if year == splitStr[1]:
-                print splitStr[0]
-    elif user_input == "7":
-        bookname = str(raw_input("enter the book"))
-        for book in bookLIST:
-            splitStr = book.split(", ")
-            if bookname == splitStr[0]:
-                print splitStr[1]
-    elif user_input == "8":
-        it = str(raw_input("enter the name"))
-        for book in bookLIST:
-            splitStr = book.split(", ")
-            if it == splitStr[0]:
-                print splitStr[2]
-    elif user_input == "9":
-        h = str(raw_input("enter the name"))
-        for book in bookLIST:
-            splitStr = book.split(", ")
-            if h == splitStr[0]:
-                if splitStr[3] == "yes":
-                    print "I have read the Book"
-                else:
-                    print "I have not read the Book"
 
+    if user_input == "1":
+ #       book_name = str(input("Name the Book "));
+ #       book_author = str(input("Name the Author "));
+ #       book_year = int(input(" Give the year of book  "));
+ #       book_Ihave = str(input(" Do i have the book "));
+ #       book = (book_name, book_author, book_year, book_Ihave);
+         book = ("abcd", "xxaqws", 1999, "yes");
+
+         add_books(connection,book);
 
 def main():
     main_input = 0
+
+    connection = DataSql.create_database("myBooks.db")
 
     while main_input != 10:
         print("What do you  like to do \n "
@@ -112,9 +64,12 @@ def main():
         main_input = input(": ")
 
         # print(type(main_input))
-        input_destinations(str(main_input))
+        input_destinations(str(main_input),connection)
 
-    myFile.close()
+        book = ("abcd", "xxaqws", 1999, "yes");
+
+        add_books(connection, book);
+
     return
 
 
