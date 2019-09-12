6e32255d013a140fcbadcaa3f3c04bf2cd1ba30e
kabeerchhllar <kabeer.chhillar@gmail.com>
2019-09-12 14:04:06 +0530

    first programs

--- hcf 3.py
+++ hcf 3.py
@@ -1,5 +1,23 @@
 import factors
 
+def fn1test():
+    rrf = []
+    for x in range(2000,3200):
+        if x % 7 == 0 and x % 5 != 0:
+            rrf.append(x)
+    return rrf
+
+def factorial(num):
+    factorial = 1
+    for x in range(1,num+1):
+        factorial = factorial * x
+    return factorial
+
+def printSquares(num):
+    fnh = []
+    for x in range(1,num+1):
+        fnh.append(x*x)
+    return fnh
 
 def hcf_3(num1,num2,num3):
     hcf = 1
@@ -12,4 +30,7 @@ def hcf_3(num1,num2,num3):
             list2.remove(x) and list3.remove(x)
     return hcf
 
-print hcf_3(2018,3027,1009)
+#print hcf_3(2018,3027,1009)
+#print fn1test()
+#print factorial(8)
+print printSquares(66546)
